62ecceb0f54732693dcfae91738949cfd5d63024
obsfeil <obsfeil@outlook.com>
2024-01-23 10:36:14 +0100

    com

--- pytontest.py
+++ pytontest.py
@@ -4,7 +4,7 @@ output_notebook()
 import numpy as np
 from bokeh_smith import smith_chart
 
-p=smith_chart(width=200,height=200)
+p= smith_chart(width=200,height=200)
 s11=np.linspace(1,.8,10)*np.exp(2*np.pi*1j*np.linspace(0,.1,10))
 p.circle(s11.real,s11.imag)
-show(p)
+show(p) # type: ignore
